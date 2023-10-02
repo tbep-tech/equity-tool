9314be6305ce86c4052138a96e1fd9ae29a47744
BAlexSimmons <b.alexander.simmons@gmail.com>
2023-10-02 11:58:34 -0400

    added data processing script (outside of app)

---
+++ R/dat_proc.R
@@ -0,0 +1,15 @@
+library(mapview)
+library(sf)
+
+load(file = 'data/tb_equity.RData')
+load(file = 'data/buffer_under.RData')
+
+buffer_under_map <- mapview(buffer_under, col.regions = "#F2AD00", layer.name = "1 mile buffer", alpha.regions = 0.5) + mapview(tb_equity, col.regions = "#00806E", layer.name = "Underserved Communities", alpha.regions = 0.5)
+
+save(buffer_under_map, compress = "xz", file = "data/buffer_under_map.RData")
+
+load(file = 'data/tb_flowlines.RData')
+
+flowlines_map <- mapview(tb_flowlines, color = "#004F7E", layer.name = "Flowlines", label = "GNIS_NAME") + mapview(tb_equity, col.regions = "#00806E", layer.name = "Underserved Communities", alpha.regions = 0.5)
+
+save(flowlines_map, compress = "xz", file = "data/flowlines_map.RData")
