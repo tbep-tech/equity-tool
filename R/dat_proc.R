b67698ceed2ec294291e516b79ad814aa42927fd
fawda123 <mbafs2012@gmail.com>
2023-10-02 16:24:53 -0400

    recreate buffer_under_map as leaflet

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,18 +1,64 @@
-library(mapview)
+library(leaflet)
 library(sf)
 
 load(file = 'data/tb_equity.RData')
 load(file = 'data/buffer_under.RData')
+newflow <- st_read("data/tb_flowlines_dissolved.shp")
 
-buffer_under_map <- mapview(buffer_under, col.regions = "#F2AD00", layer.name = "1 mile buffer", alpha.regions = 0.5) + mapview(tb_equity, col.regions = "#00806E", layer.name = "Underserved Communities", alpha.regions = 0.5)
+buffer_under <- st_transform(buffer_under, crs = 4326)
+tb_equity <- st_transform(tb_equity, crs = 4326)
+newflow <- st_transform(newflow, crs = 4326)
 
-buffer_under_map <- buffer_under_map@map
+# bounding box
+bnds <- sf::st_bbox(newflow)
+
+# esri tiles
+esri <- rev(grep("^Esri", leaflet::providers, value = TRUE))
+
+# empty map
+m <- leaflet::leaflet() %>%
+  leaflet::fitBounds(bnds[['xmin']], bnds[['ymin']], bnds[['xmax']], bnds[['ymax']]) %>%
+  leaflet::addScaleBar(position = "bottomleft")
+
+# add provider tiles
+for (provider in esri) {
+  m <- m %>% leaflet::addProviderTiles(provider, group = provider)
+}
+
+# buffer under map ----------------------------------------------------------------------------
+
+buffer_under_map <- m %>%
+  addPolygons(
+    data = buffer_under,
+    group = "1 mile buffer",
+    fillColor = "#F2AD00",
+    color = 'black',
+    fillOpacity = 0.5,
+    weight = 1,
+    opacity = 1
+    ) %>%
+  addPolygons(
+    data = tb_equity,
+    group = "Underserved Communities",
+    fillColor = "#00806E",
+    color = 'black',
+    fillOpacity = 0.5,
+    weight = 1,
+    opacity = 1
+  ) %>%
+  addLegend(labels = "1 mile buffer", colors = "#F2AD00", opacity = 1) %>%
+  addLegend(labels = "Underserved Communities", colors = "#00806E", opacity = 1) %>%
+  addLayersControl(
+    baseGroups = names(esri),
+    overlayGroups = c("1 mile buffer", "Underserved Communities"),
+    position = 'topleft'
+    )
 
 save(buffer_under_map, compress = "xz", file = "data/buffer_under_map.RData")
 
-load(file = 'data/tb_flowlines.RData')
+# flowlines map -------------------------------------------------------------------------------
 
-newflow <- st_read("data/tb_flowlines_dissolved.shp")
+
 
 tb_flowlines_fixed <- st_make_valid(newflow)
 
